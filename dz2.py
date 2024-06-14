65be73a28de1e44b53ebc0ecb899b2aec5781f73
Budilnick <msmaksimov1@gmail.com>
2024-06-14 18:09:17 +0300

    Initial commit

---
+++ dz2.py
@@ -0,0 +1,4 @@
+name = "Maksim"
+surename = "Maksimov"
+lastname = "Stanislavovich"
+print("ФИО - " + surename + " " + name[0] + "." + lastname[0] + ".")
---
+++ main.py
@@ -0,0 +1,2 @@
+a = 'homework'
+print(len(a))
